e80e60d88cc435ffbe957a573651de4da48c4d39
Kimiyuki Onaka <kimiyuki95@gmail.com>
2017-08-08 21:58:05 +0900

    141672m

--- a.cpp
+++ a.cpp
@@ -1,5 +1,7 @@
 #include <cstdio>
 #include <cmath>
+#include <random>
+#include <chrono>
 #include <vector>
 #include <algorithm>
 #include <array>
@@ -26,32 +28,23 @@ template <class T> inline void setmin(T & a, T const & b) { a = min(a, b); }
 template <typename X, typename T> auto vectors(X x, T a) { return vector<T>(x, a); }
 template <typename X, typename Y, typename Z, typename... Zs> auto vectors(X x, Y y, Z z, Zs... zs) { auto cont = vectors(y, z, zs...); return vector<decltype(cont)>(x, cont); }
 
-double calculate_length(vector<int> const & path, vector<vector<double> > const & dist) {
-    double acc = 0;
-    repeat (i, path.size() - 1) {
-        acc += dist[path[i]][path[i + 1]];
-    }
-    return acc;
-}
 
-constexpr int root = 0;
-int main() {
-    // input
-    int n; scanf("%d", &n);
-    vector<int> y(n), x(n);
-    repeat (i, n) scanf("%d%d", &y[i], &x[i]);
-    // solve
-    auto dist = vectors(n + 1, n + 1, double());
-    dist[root][root] = 0;
-    repeat (i, n) {
-        dist[root][i + 1] = hypotl(y[i], x[i]);
-        dist[i + 1][root] = hypotl(y[i], x[i]);
-        repeat (j, n) {
-            dist[i + 1][j + 1] = hypotl(y[j] - y[i], x[j] - x[i]);
-        }
-    }
+chrono::high_resolution_clock::time_point clock_begin;
+bool check_tle() {
+    chrono::high_resolution_clock::time_point clock_end = chrono::high_resolution_clock::now();
+    return chrono::duration_cast<chrono::milliseconds>(clock_end - clock_begin).count() >= 2900;
+}
+default_random_engine gen;
+
+constexpr double eps = 1e-6;
+constexpr int MAX_N = 100;
+int n;
+double dist[MAX_N + 2][MAX_N + 2];
+double calculate_length(vector<int> const & path); // for debug
+vector<int> solve_greedy() {
     vector<int> path(n + 1);
     vector<bool> used(n + 1);
+    constexpr int root = 0;
     path[0] = root;
     used[root] = true;
     repeat (len, n) {
@@ -66,11 +59,85 @@ int main() {
         path[len + 1] = k;
         used[k] = true;
     }
+    return path;
+}
+vector<int> solve_swap() {
+    // vector<int> path(n + 2);
+    // iota(whole(path), 0);
+    // shuffle(path.begin() + 1, path.begin() + (n + 1), gen);
+    vector<int> path = solve_greedy();
+    path.push_back(n + 1);
+    for (int iteration = 0; ; ++ iteration) {
+        bool modified = false;
+        repeat_from (i, 1, n) {
+            repeat_from (j, i + 1, n + 1) {
+                double delta = 0;
+                delta -= dist[path[i - 1]][path[i]] + dist[path[i]][path[i + 1]];
+                delta -= dist[path[j - 1]][path[j]] + dist[path[j]][path[j + 1]];
+                swap(path[i], path[j]);
+                delta += dist[path[i - 1]][path[i]] + dist[path[i]][path[i + 1]];
+                delta += dist[path[j - 1]][path[j]] + dist[path[j]][path[j + 1]];
+                if (delta < - eps) {
+                    modified = true;
+                } else {
+                    swap(path[i], path[j]);
+                }
+            }
+        }
+        if (not modified) break;
+        if (iteration % 128 == 0 and check_tle()) break;
+    }
+    assert (path.back() == n + 1);
+    path.pop_back();
+    return path;
+}
+
+double calculate_length(vector<int> const & path) {
+    double acc = 0;
+    repeat (i, path.size() - 1) {
+        acc += dist[path[i]][path[i + 1]];
+    }
+    return acc;
+}
+
+vector<int> solve(int a_n, vector<int> const & y, vector<int> const & x) {
+    assert (a_n <= MAX_N);
+    // prepare
+    clock_begin = chrono::high_resolution_clock::now();
+    n = a_n;
+    constexpr int root = 0;
+    dist[root][root] = 0;
+    repeat (i, n) {
+        dist[root][i + 1] = hypot(y[i], x[i]);
+        dist[i + 1][root] = hypot(y[i], x[i]);
+        repeat (j, n) {
+            dist[i + 1][j + 1] = hypot(y[j] - y[i], x[j] - x[i]);
+        }
+        dist[n + 1][i + 1] = 0;
+        dist[i + 1][n + 1] = 0;
+    }
+    // solve
+    vector<int> path = solve_swap();
+    assert (path.size() == n + 1);
+    // result
+    fprintf(stderr, "result: %lfm\n", calculate_length(path));
+    assert (path[0] == 0);
+    repeat (i, n) path[i] = path[i + 1] - 1;
+    path.pop_back();
+    return path;
+}
+
+int main() {
+    // input
+    int n; scanf("%d", &n);
+    vector<int> y(n), x(n);
+    repeat (i, n) scanf("%d%d", &y[i], &x[i]);
+    // solve
+    vector<int> path = solve(n, y, x);
     // output
     repeat (i, n) {
-        int j = path[i + 1] - 1;
+        int j = path[i];
         printf("%d %d\n", y[j], x[j]);
     }
-    fprintf(stderr, "result: %lfm\n", calculate_length(path, dist));
     return 0;
 }
--- readme.md
+++ readme.md
@@ -3,3 +3,8 @@
 <https://paiza.jp/poh/phantom_thief>
 
 御社の金で焼き肉が食べたい！
+
+## メモ
+
+-   単純greedy: 144850m
+-   単純greedy + swap: 141672m
